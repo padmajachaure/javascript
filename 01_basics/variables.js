1f3a698def1b88b0d85c2f174eb01edc1b78e3bb
padmajachaure <163647039+padmajachaure@users.noreply.github.com>
2025-06-21 15:40:31 +0000

    variables & datatyepes

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 12322 // define constant
+let accountEmail = "Padma@123gmail.com"
+var password = 765677656789
+accCity = "Pune"
+
+accountEmail = "Padmaja@123gmail.com"
+password = "123456543212345"
+accCity = "Solapur"
+
+console.table([accountEmail, accountId , password , accCity])
+
+
+// prefer not to use var for variable declaration
+// because of issue in block of scope and functional scope
